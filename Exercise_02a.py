9b0295242af0837a8bffd88ecec5f112b27743e3
Lyu-Wei <weilyu@whu.edu.cn>
2016-09-16 14:46:53 +0800

    Add files via upload

---
+++ Exercise_02a.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 14:36:35 2016
+
+@author: lenovo
+"""
+
+print '#    \n#    \n#    \n#    \n#####'
---
+++ Exercise_02b.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 13:27:17 2016
+
+@author: lenovo
+"""
+
+a = raw_input('press enter to print my name ^_^ ')
+print '#     #   # #   #    #   # #####   #  '
+print '#      # #  #   #    #   # #       #  '
+print '#       #   #   #    # # # #####   #  '
+print '#       #   #   #    ## ## #       #  '
+print '#####   #   #####    #   # #####   #  '
